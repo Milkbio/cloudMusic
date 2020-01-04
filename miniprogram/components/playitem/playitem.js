163104e6e7be5cdf652980a6d81ddf8ca74ab02b
Milkbio <1032635701@qq.com>
2020-01-05 01:03:47 +0800

    更新

---
+++ miniprogram/components/playitem/playitem.js
@@ -0,0 +1,40 @@
+Component({
+  /**
+   * 组件的属性列表
+   */
+  properties: {
+    playItem: {
+      type: Object
+    }
+  },
+  observers: {
+    ['playItem.playCount'](val) {
+      this.setData({
+        count: this.formatCount(val)
+      })
+      this.formatCount(val)
+    }
+  },
+  /**
+   * 组件的初始数据
+   */
+  data: {
+    count: 0
+  },
+
+  /**
+   * 组件的方法列表
+   */
+  methods: {
+    formatCount(count) {
+      const unitArr = ['', '万', '亿']
+      let index = 0
+      let floatCount = parseFloat(count)
+      index = Math.floor(Math.log(floatCount) / Math.log(10000))
+
+      let result = floatCount / Math.pow(10000, index)
+      result = result.toFixed(2)
+      return result + unitArr[index]
+    }
+  }
+})
